fffdf325805c41eba7a0008f4fb04cd95ead13ae
lokeshkavisth <kavisthlokesh@gmail.com>
2025-02-13 17:22:52 +0530

    style(template): fix dark mode and spacing issues, ui improvements

--- src/components/resume/templates/template-1.jsx
+++ src/components/resume/templates/template-1.jsx
@@ -1,5 +1,5 @@
 import { Badge } from "@/components/ui/badge";
-import { Card, CardContent } from "@/components/ui/card";
+// import { div, div } from "@/components/ui/ div";
 import { Separator } from "@/components/ui/separator";
 import { MoveUpRight } from "lucide-react";
 
@@ -20,16 +20,16 @@ const TemplateOne = ({ resumeData }) => {
   } = resumeData;
 
   return (
-    <div className="max-w-3xl mx-auto p-6 bg-white border border-dashed rounded-lg">
+    <div className="max-w-4xl mx-auto p-6 bg-background border border-dashed rounded-lg">
       {/* Header Section */}
-      <div className="text-center">
+      <div className="text-center ">
         <h1 className="text-3xl font-bold">{name}</h1>
-        <p className="text-lg text-gray-600">{jobRole}</p>
+        <p className="text-lg">{jobRole}</p>
       </div>
       <Separator className="my-4" />
 
       {/* Contact & Socials */}
-      <div className="flex justify-between text-sm text-gray-600">
+      <div className="flex justify-between text-sm">
         <p>Email: {contact.email}</p>
         <p>Phone: {contact.phone}</p>
         <p>Location: {contact.location}</p>
@@ -39,7 +39,7 @@ const TemplateOne = ({ resumeData }) => {
           <a
             key={index}
             href={link.url}
-            className="text-blue-600 hover:underline"
+            className="text-blue-600/80 hover:underline"
           >
             {link.title}
           </a>
@@ -48,106 +48,119 @@ const TemplateOne = ({ resumeData }) => {
       <Separator className="my-4" />
 
       {/* Profile Summary */}
-      <Card className="border-none shadow-none">
-        <CardContent className="p-4">
-          <h2 className="text-xl font-semibold">Profile Summary</h2>
-          <p className="text-gray-700 mt-2">{profileSummary}</p>
-        </CardContent>
-      </Card>
+      <div className="border-none shadow-none">
+        <h2 className="text-xl font-semibold">Profile Summary</h2>
+        <p className="text-muted-foreground mt-2">{profileSummary}</p>
+      </div>
       <Separator className="my-4" />
 
       {/* Experience */}
       <h2 className="text-xl font-semibold">Experience</h2>
       {experience.map((exp, index) => (
-        <Card key={index} className="border-none shadow-none">
-          <CardContent className="p-4">
+        <div key={index} className="border-none shadow-none">
+          <div className="p-4">
             <h3 className="font-semibold">
               {exp.jobRole} - {exp.companyName}
             </h3>
-            <p className="text-sm text-gray-500">
+            <p className="text-sm text-muted-foreground/90">
               {exp.startDate} - {exp.endDate}
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-muted-foreground mt-2">
               {exp.description.map((desc, i) => (
                 <li key={i}>{desc}</li>
               ))}
             </ul>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       ))}
       <Separator className="my-4" />
 
       {/* Education */}
       <h2 className="text-xl font-semibold">Education</h2>
       {education.map((edu, index) => (
-        <Card key={index} className="border-none shadow-none">
-          <CardContent className="p-4">
+        <div key={index} className="border-none shadow-none">
+          <div className="p-4">
             <h3 className="font-semibold">{edu.universityName}</h3>
-            <p className="text-sm text-gray-500">
+            <p className="text-sm text-muted-foreground">
               {edu.degreeTitle} • {edu.startYear} - {edu.endYear}
             </p>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       ))}
       <Separator className="my-4" />
 
       {/* Projects */}
       <h2 className="text-xl font-semibold">Projects</h2>
       {projects.map((proj, index) => (
-        <Card key={index} className="border-none shadow-none">
-          <CardContent className="p-4">
+        <div key={index} className="border-none shadow-none">
+          <div className="p-4">
             <h3 className="font-semibold">{proj.title}</h3>
-            <p className="text-sm text-gray-500">
+            <p className="text-sm text-muted-foreground/90">
               {proj.startDate} - {proj.endDate || "Present"}
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-muted-foreground mt-2">
               {proj.description.map((desc, i) => (
                 <li key={i}>{desc}</li>
               ))}
             </ul>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       ))}
       <Separator className="my-4" />
 
       {/* Skills */}
       <h2 className="text-xl font-semibold">Technical Skills</h2>
-      <div className="flex flex-wrap gap-2 mt-2">
+      <div className="flex flex-wrap gap-2 p-4">
         {techSkills.map((skill, index) => (
-          <Badge key={index}>{skill}</Badge>
+          <Badge
+            key={index}
+            className={
+              "bg-muted text-foreground hover:bg-muted hover:text-foreground shadow-none !border"
+            }
+          >
+            {skill}
+          </Badge>
         ))}
       </div>
       <Separator className="my-4" />
 
       {/* Achievements & Courses */}
       <h2 className="text-xl font-semibold">Achievements</h2>
-      {achievements.map((ach, index) => (
-        <p key={index} className="text-gray-700">
-          • {ach.title}: {ach.description}
-        </p>
-      ))}
+      <div className="p-4">
+        <ul className="list-disc list-inside text-muted-foreground">
+          {achievements.map((ach, i) => (
+            <li key={i}>
+              {ach.title}: {ach.description}
+            </li>
+          ))}
+        </ul>
+      </div>
       <Separator className="my-4" />
+
       <h2 className="text-xl font-semibold">Courses</h2>
-      {courses.map((course, index) => (
-        <p key={index} className="text-gray-700 flex items-center gap-3">
-          • {course.title}
-          <a href={course.link} className="text-blue-600 hover:underline">
-            <MoveUpRight className="size-4" />
-          </a>
-        </p>
-      ))}
+      <div className="p-4">
+        {courses.map((course, index) => (
+          <p
+            key={index}
+            className="text-muted-foreground flex items-center gap-3"
+          >
+            • {course.title}
+            <a href={course.link} className="text-blue-600/80 hover:underline">
+              <MoveUpRight className="size-4" />
+            </a>
+          </p>
+        ))}
+      </div>
       <Separator className="my-4" />
 
       {/* Passion */}
       {passion && (
-        <Card className="border-none shadow-none">
-          <CardContent className="p-4">
-            <h2 className="text-xl font-semibold">Passion</h2>
-            <p className="text-gray-700 mt-2">
-              {passion.title}: {passion.description}
-            </p>
-          </CardContent>
-        </Card>
+        <div className="border-none shadow-none">
+          <h2 className="text-xl font-semibold">Passion</h2>
+          <p className="text-muted-foreground mt-2">
+            {passion.title}: {passion.description}
+          </p>
+        </div>
       )}
     </div>
   );
--- src/components/resume/templates/template-2.jsx
+++ src/components/resume/templates/template-2.jsx
@@ -1,49 +1,50 @@
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Mail, Phone, MapPin, Globe } from "lucide-react";
+import { Mail, Phone, MapPin, Globe, MoveUpRight } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 const ResumeTemplate = ({ resume }) => {
   return (
-    <div className="max-w-4xl mx-auto bg-background border border-dashed rounded-lg">
+    <div className="max-w-5xl mx-auto bg-background border border-dashed rounded-lg">
       <div className="grid grid-cols-3 gap-6">
         {/* Left Column (Personal Details) */}
 
-        <div className="col-span-1 space-y-6 py-6 pl-6 rounded-lg bg-primary-foreground">
+        <div className="col-span-1 space-y-6 p-6 rounded-lg bg-primary-foreground">
           {/* Header */}
-          <div className="text-center p-4">
+          <div className="text-center py-4">
             <h1 className="text-3xl font-bold">{resume.name}</h1>
-            <p className="text-lg text-gray-600">{resume.jobRole}</p>
+            <p className="text-lg ">{resume.jobRole}</p>
           </div>
 
           {/* Contact */}
           <Card className="border-none shadow-none bg-transparent">
-            <CardContent className="p-4 space-y-2">
+            <CardContent className="p-4 px-0 space-y-2">
               <h2 className="text-xl font-semibold">Contact</h2>
               <Separator />
-              <p className="flex items-center gap-2 text-gray-700">
-                <Mail className="w-4 h-4" /> {resume.contact.email}
+              <p className="flex items-center gap-2 text-muted-foreground">
+                <Mail className="size-4" /> {resume.contact.email}
               </p>
-              <p className="flex items-center gap-2 text-gray-700">
-                <Phone className="w-4 h-4" /> {resume.contact.phone}
+              <p className="flex items-center gap-2 text-muted-foreground">
+                <Phone className="size-4" /> {resume.contact.phone}
               </p>
-              <p className="flex items-center gap-2 text-gray-700">
-                <MapPin className="w-4 h-4" /> {resume.contact.location}
+              <p className="flex items-center gap-2 text-muted-foreground">
+                <MapPin className="size-4" /> {resume.contact.location}
               </p>
             </CardContent>
           </Card>
 
           {/* Social Links */}
           <Card className="border-none shadow-none bg-transparent">
-            <CardContent className="p-4 space-y-2">
+            <CardContent className="p-4 px-0 space-y-2">
               <h2 className="text-xl font-semibold">Social Links</h2>
               <Separator />
               {resume.socialLinks.map((link, index) => (
                 <p
                   key={index}
-                  className="flex items-center gap-2 text-blue-500"
+                  className="flex items-center gap-2 text-blue-600/80"
                 >
-                  <Globe className="w-4 h-4" />
+                  <Globe className="size-4" />
                   <a href={link.url} target="_blank" rel="noopener noreferrer">
                     {link.title}
                   </a>
@@ -54,37 +55,71 @@ const ResumeTemplate = ({ resume }) => {
 
           {/* Skills */}
           <Card className="border-none shadow-none bg-transparent">
-            <CardContent className="p-4">
+            <CardContent className="py-4 px-0">
               <h2 className="text-xl font-semibold">Skills</h2>
               <Separator />
               <div className="flex flex-wrap gap-2 mt-2">
                 {resume.techSkills.map((skill, index) => (
-                  <span
-                    key={index}
-                    className="bg-gray-200 text-gray-700 text-sm px-3 py-1 rounded-lg"
-                  >
+                  <Badge key={index} className="font-medium">
                     {skill}
-                  </span>
+                  </Badge>
                 ))}
               </div>
             </CardContent>
           </Card>
+
+          {/* Courses */}
+          <Card className="border-none shadow-none bg-transparent">
+            <CardContent className="py-4 px-0">
+              <h2 className="text-xl font-semibold">Courses</h2>
+              <Separator />
+              <div className="flex flex-wrap gap-2 mt-2">
+                <ul className="list-disc text-muted-foreground p-4">
+                  {resume.courses.map((course, index) => (
+                    <li key={index}>
+                      <div className="flex items-center gap-2">
+                        {course.title}
+                        <a
+                          href={course.link}
+                          className="text-blue-600/80 hover:underline"
+                        >
+                          <MoveUpRight className="size-4" />
+                        </a>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </CardContent>
+          </Card>
+
+          {/* Passion */}
+
+          {resume.passion && (
+            <Card className="border-none shadow-none bg-transparent">
+              <h2 className="text-xl font-semibold">Passion</h2>
+              <Separator />
+              <p className="text-muted-foreground py-4">
+                {resume.passion.title}: {resume.passion.description}
+              </p>
+            </Card>
+          )}
         </div>
 
         {/* Right Column (Experience, Education, Projects) */}
         <div className="col-span-2 space-y-6 py-6 pr-6">
           {/* Experience */}
           <Card className="border-none shadow-none">
-            <CardContent className="p-4">
+            <CardContent className="p-4 px-0">
               <h2 className="text-xl font-semibold">Experience</h2>
               <Separator />
               {resume.experience.map((exp, index) => (
                 <div key={index} className="mt-4">
                   <h3 className="text-lg font-semibold">{exp.companyName}</h3>
-                  <p className="text-sm text-gray-600">
+                  <p className="text-sm text-muted-foreground/90">
                     {exp.jobRole} • {exp.startDate} - {exp.endDate}
                   </p>
-                  <ul className="list-disc list-inside mt-1 text-gray-700">
+                  <ul className="list-disc list-inside text-muted-foreground p-4">
                     {exp.description.map((desc, i) => (
                       <li key={i}>{desc}</li>
                     ))}
@@ -96,15 +131,15 @@ const ResumeTemplate = ({ resume }) => {
 
           {/* Education */}
           <Card className="border-none shadow-none">
-            <CardContent className="p-4">
+            <CardContent className="p-4 px-0">
               <h2 className="text-xl font-semibold">Education</h2>
               <Separator />
               {resume.education.map((edu, index) => (
-                <div key={index} className="mt-4">
+                <div key={index} className="p-4">
                   <h3 className="text-lg font-semibold">
                     {edu.universityName}
                   </h3>
-                  <p className="text-sm text-gray-600">
+                  <p className="text-sm text-muted-foreground/90">
                     {edu.degreeTitle} • {edu.startYear} - {edu.endYear}
                   </p>
                 </div>
@@ -114,18 +149,18 @@ const ResumeTemplate = ({ resume }) => {
 
           {/* Projects */}
           <Card className="border-none shadow-none">
-            <CardContent className="p-4">
+            <CardContent className="p-4 px-0">
               <h2 className="text-xl font-semibold">Projects</h2>
               <Separator />
               {resume.projects.map((project, index) => (
                 <div key={index} className="mt-4">
                   <h3 className="text-lg font-semibold">{project.title}</h3>
                   {project.companyName && (
-                    <p className="text-sm text-gray-600">
+                    <p className="text-sm text-muted-foreground/90">
                       {project.companyName}
                     </p>
                   )}
-                  <ul className="list-disc list-inside mt-1 text-gray-700">
+                  <ul className="list-disc list-inside p-4 text-muted-foreground">
                     {project.description.map((desc, i) => (
                       <li key={i}>{desc}</li>
                     ))}
